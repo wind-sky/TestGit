2a1398889cf34748da51bcaa18df250bc264fe65
yuwenjie <yuwenjie@cetc-cloud.com>
2017-08-22 15:57:22 +0800

    add new file TestGit

---
+++ src/main/java/com/cetc/cloud/TestGit.java
@@ -0,0 +1,14 @@
+package com.cetc.cloud;
+
+/**
+ * @ClassName: com.cetc.cloud.TestGit
+ * @Description:
+ * @author: Yu Wenjie
+ * @version: V1.0
+ * @date: 2017/8/22 15:51
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("This is my first use git to management version control!");
+    }
+}
